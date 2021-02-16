290f42fc246cbf6149b878f5a6eed77a87c39be7
97kovacspeter <>
2021-02-16 21:53:37 +0100

    reupload

---
+++ Robot/View/GameForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Robot
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container( );
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( GameForm ) );
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip( );
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.continueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.x7ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.x11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.x15ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem( );
+            this.timer1 = new System.Windows.Forms.Timer( this.components );
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog( );
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog( );
+            this.label1 = new System.Windows.Forms.Label( );
+            this.menuStrip1.SuspendLayout( );
+            this.SuspendLayout( );
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange( new System.Windows.Forms.ToolStripItem[ ] {
+            this.pauseToolStripMenuItem,
+            this.continueToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.newGameToolStripMenuItem} );
+            this.menuStrip1.Location = new System.Drawing.Point( 0, 0 );
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size( 584, 24 );
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size( 50, 20 );
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler( this.pauseToolStripMenuItem_Click );
+            // 
+            // continueToolStripMenuItem
+            // 
+            this.continueToolStripMenuItem.Enabled = false;
+            this.continueToolStripMenuItem.Name = "continueToolStripMenuItem";
+            this.continueToolStripMenuItem.Size = new System.Drawing.Size( 68, 20 );
+            this.continueToolStripMenuItem.Text = "Continue";
+            this.continueToolStripMenuItem.Click += new System.EventHandler( this.continueToolStripMenuItem_Click_1 );
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Enabled = false;
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size( 45, 20 );
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler( this.loadToolStripMenuItem_Click_1 );
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size( 43, 20 );
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler( this.saveToolStripMenuItem_Click_1 );
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.DropDownItems.AddRange( new System.Windows.Forms.ToolStripItem[ ] {
+            this.x7ToolStripMenuItem,
+            this.x11ToolStripMenuItem,
+            this.x15ToolStripMenuItem} );
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size( 77, 20 );
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler( this.newGameToolStripMenuItem_Click_1 );
+            // 
+            // x7ToolStripMenuItem
+            // 
+            this.x7ToolStripMenuItem.Name = "x7ToolStripMenuItem";
+            this.x7ToolStripMenuItem.Size = new System.Drawing.Size( 180, 22 );
+            this.x7ToolStripMenuItem.Text = "7x7";
+            this.x7ToolStripMenuItem.Click += new System.EventHandler( this.x7ToolStripMenuItem_Click );
+            // 
+            // x11ToolStripMenuItem
+            // 
+            this.x11ToolStripMenuItem.Name = "x11ToolStripMenuItem";
+            this.x11ToolStripMenuItem.Size = new System.Drawing.Size( 180, 22 );
+            this.x11ToolStripMenuItem.Text = "11x11";
+            this.x11ToolStripMenuItem.Click += new System.EventHandler( this.x11ToolStripMenuItem_Click );
+            // 
+            // x15ToolStripMenuItem
+            // 
+            this.x15ToolStripMenuItem.Name = "x15ToolStripMenuItem";
+            this.x15ToolStripMenuItem.Size = new System.Drawing.Size( 180, 22 );
+            this.x15ToolStripMenuItem.Text = "15x15";
+            this.x15ToolStripMenuItem.Click += new System.EventHandler( this.x15ToolStripMenuItem_Click );
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler( this.timer1_Tick );
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Robot table (*.rtl)|*.rtl";
+            this.openFileDialog1.Title = "Loading Robot table";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Robot table (*.rtl)|*.rtl";
+            this.saveFileDialog1.Title = "Saving Robot table";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point( 290, 5 );
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size( 35, 13 );
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size( 584, 561 );
+            this.Controls.Add( this.label1 );
+            this.Controls.Add( this.menuStrip1 );
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject( "$this.Icon" )));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GameForm";
+            this.Text = "Crazy Robot";
+            this.Load += new System.EventHandler( this.GameForm_Load );
+            this.menuStrip1.ResumeLayout( false );
+            this.menuStrip1.PerformLayout( );
+            this.ResumeLayout( false );
+            this.PerformLayout( );
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem continueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem x7ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x11ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x15ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Robot/View/GameForm.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Robot.Model;
+using Robot.Persistence;
+
+
+namespace Robot
+{
+    public partial class GameForm : Form
+    {
+        #region Fields
+
+        private IDataAccess _dataAccess;
+        private GameModel _model;
+        private Button[ , ] _gameButtons;
+        private Timer _timer;
+        private int _size = 9;
+
+        #endregion
+
+        #region Constructors
+
+        public GameForm()
+        {
+            InitializeComponent( );
+        }
+
+        private void GameForm_Load( object sender, EventArgs e )
+        {
+            _dataAccess = new FileDataAccess( );
+            _model = new GameModel( _dataAccess );
+            GenerateTable( _size );
+
+            // időzítő létrehozása
+            _timer = new Timer
+            {
+                Interval = 100
+            };
+            label1.Text = "TIMER: 0";
+            _timer.Tick += new EventHandler( timer1_Tick );
+
+            ViewUpdate( this, null );
+            _model.GameAdvanced += new EventHandler( ViewUpdate );
+            _model.GameOver += new EventHandler( GameOver );
+            _timer.Start( );
+        }
+
+        #endregion
+
+        #region Timer
+
+        private void timer1_Tick( object sender, EventArgs e )
+        {
+            _model.AdvanceTime( ); // játék léptetése
+        }
+
+        #endregion
+
+        #region Table generating
+
+        /// <summary>
+        /// Tábla generálás
+        /// </summary>
+        private void GenerateTable( int size )
+        {
+            if (_gameButtons != null)
+            {
+                for (int i = 0; i < _gameButtons.GetLength( 0 ); ++i)
+                {
+                    for (int j = 0; j < _gameButtons.GetLength( 1 ); ++j)
+                    {
+                        Controls.Remove( _gameButtons[ i, j ] );
+                    }
+                }
+            }
+            _gameButtons = new Button[ size, size ];
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    _gameButtons[ i, j ] = new Button
+                    {
+                        Location = new Point( 5 + 40 * j, 20 + 40 * i ),
+                        Size = new Size( 40, 40 ),
+                        TabIndex = 100 + i * 10 + j
+                    };
+                    _gameButtons[ i, j ].MouseClick += new MouseEventHandler( ButtonGrid_MouseClick );
+
+                    Controls.Add( _gameButtons[ i, j ] );
+                }
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        public void ViewUpdate( object sender, EventArgs e )
+        {
+            label1.Text = "TIMER: " + (_model.GameTime / 10).ToString( ) + ":" + (_model.GameTime % 10).ToString( );
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+
+                    if (_model.Table.FieldValues[ i, j ] == FieldType.PermaWall)
+                    {
+                        _gameButtons[ i, j ].BackgroundImage = (Image)(Properties.Resources.perma);
+                    }
+                    else if (_model.Table.FieldValues[ i, j ] == FieldType.Wall)
+                    {
+                        _gameButtons[ i, j ].BackgroundImage = (Image)(Properties.Resources.wall);
+                    }
+                    else if (_model.Table.FieldValues[ i, j ] == FieldType.Ruined)
+                    {
+                        _gameButtons[ i, j ].BackgroundImage = (Image)(Properties.Resources.ruined);
+                    }
+                    else if (_model.Table.FieldValues[ i, j ] == FieldType.Magnet)
+                    {
+                        _gameButtons[ i, j ].BackgroundImage = (Image)(Properties.Resources.magnet);
+                    }
+                    else if (_model.Table.FieldValues[ i, j ] == FieldType.Empty)
+                    {
+                        _gameButtons[ i, j ].BackColor = Color.White;
+                        _gameButtons[ i, j ].BackgroundImage = null;
+                    }
+
+                }
+            }
+
+            _gameButtons[ _model.PlayerXValue, _model.PlayerYValue ].BackgroundImage = Properties.Resources.robot;
+
+        }
+
+        #endregion
+
+        #region GameOver
+
+        public void GameOver( object sender, EventArgs e )
+        {
+
+            if (MessageBox.Show( "You won! Your time is: " + _model.GameTime / 10 + ":" + _model.GameTime % 10 + " Want to start new game?", "You won!", MessageBoxButtons.YesNo, MessageBoxIcon.Question ) == DialogResult.Yes)
+            {
+                _model.NewGame( _size );
+            }
+            else
+            {
+                _model.Pause( );
+            }
+
+
+        }
+
+        #endregion
+
+        #region Grid event handlers
+
+        /// <summary>
+        /// Gombrács eseménykezelője.
+        /// </summary>
+        private void ButtonGrid_MouseClick( Object sender, MouseEventArgs e )
+        {
+            // a TabIndex-ből megkapjuk a sort és oszlopot
+            int x = ((sender as Button).TabIndex - 100) / 10;
+            int y = ((sender as Button).TabIndex - 100) % 10;
+
+            FieldType value = _model.Table.FieldValues[ x, y ];
+
+            bool check = CheckWall( value );
+
+            if (x == _model.PlayerXValue && y == _model.PlayerYValue)
+            {
+                check = false;
+            }
+            if (check)
+            {
+                _model.Table.FieldValues[ x, y ] = FieldType.Wall;
+            }
+            ViewUpdate( this, null );
+        }
+
+        private bool CheckWall( FieldType value )
+        {
+            switch (value)
+            {
+                case FieldType.Empty:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        #endregion
+
+        #region Menu events
+
+
+        private void x7ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            _size = 9;
+            _model.NewGame( _size );
+            GenerateTable( _size );
+        }
+
+        private void x11ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            _size = 13;
+            _model.NewGame( _size );
+            GenerateTable( _size );
+        }
+
+        private void x15ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            _size = 17;
+            _model.NewGame( _size );
+            GenerateTable( _size );
+        }
+
+        private void pauseToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            _timer.Stop( );
+            _model.Pause( );
+
+            pauseToolStripMenuItem.Enabled = false;
+            continueToolStripMenuItem.Enabled = true;
+            loadToolStripMenuItem.Enabled = true;
+            saveToolStripMenuItem.Enabled = true;
+        }
+
+        private void continueToolStripMenuItem_Click_1( object sender, EventArgs e )
+        {
+            _timer.Start( );
+            _model.Continue( );
+            pauseToolStripMenuItem.Enabled = true;
+            continueToolStripMenuItem.Enabled = false;
+            loadToolStripMenuItem.Enabled = false;
+            saveToolStripMenuItem.Enabled = false;
+        }
+
+        private async void loadToolStripMenuItem_Click_1( object sender, EventArgs e )
+        {
+            _timer.Stop( );
+
+            if (openFileDialog1.ShowDialog( ) == DialogResult.OK)
+            {
+                try
+                {
+                    await _model.LoadGameAsync( openFileDialog1.FileName );
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show( "Játék betöltése sikertelen!" + Environment.NewLine + "Hibás az elérési út, vagy a fájlformátum.", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    _model.NewGame( _size );
+                }
+            }
+
+            _size = _model.ModelSize;
+            _model.Pause( );
+            GenerateTable( _size );
+            ViewUpdate( this, null );
+        }
+
+        private async void saveToolStripMenuItem_Click_1( object sender, EventArgs e )
+        {
+            _timer.Stop( );
+
+            if (saveFileDialog1.ShowDialog( ) == DialogResult.OK)
+            {
+                try
+                {
+
+                    await _model.SaveGameAsync( saveFileDialog1.FileName );
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show( "Játék mentése sikertelen!" + Environment.NewLine + "Hibás az elérési út, vagy a könyvtár nem írható.", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+            }
+            _model.Pause( );
+        }
+
+        private void newGameToolStripMenuItem_Click_1( object sender, EventArgs e )
+        {
+
+        }
+
+        #endregion
+    }
+}
